358d5ddb0fb2a702222c7c26f4ee959b6a49a82b
lynnethP <lynnethpereira12@gmail.com>
2023-09-21 18:24:01 -0400

    Changes prueba

--- projects/prueba_tecnica/src/App.jsx
+++ projects/prueba_tecnica/src/App.jsx
@@ -5,14 +5,25 @@ export function App() {
     // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firtsWord}?size=50&color=red&&json=true`
     const [fact, setFact] = useState()
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)        
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
-            .then(data => setFact(data.fact))
+            .then(data => {
+                const {fact} = data
+                setFact(fact)
+                const threeFirstWords = fact.split(' ', 3).join(' ')
+                console.log(threeFirstWords)
+                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&&json=true`)
+                    .then(res => res.json())
+                    .then(response => {
+                        console.log(response) 
+                        const {url} = response
+                    })
+            })
     }, [])
     return (
         <main>
             <h1>App de gatitos</h1>
-            {fact && <p>{fact}</p>}          
+            {fact && <p>{fact}</p>}
         </main>
     )
 }
