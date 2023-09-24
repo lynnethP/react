7a52fe399f28bf52ce85cf6368f249b0d00dcfac
lynnethP <lynnethpereira12@gmail.com>
2023-09-24 18:50:44 -0400

    Finalizando los requerimientos solicitados

--- projects/prueba_tecnica/src/App.jsx
+++ projects/prueba_tecnica/src/App.jsx
@@ -1,29 +1,58 @@
 import { useEffect, useState } from "react"
+import './App.css'
+import { getRandomFact } from "./services/fact.js"
+
+
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firtsWord}?size=50&color=red&&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+function useCatImage({fact}) {
+    
+    const [imageUrl, setImageUrl] = useState()
+
+    //Recuperar imagen cada vez que se tiene una cita
+    useEffect(() => {
+        if (!fact) return
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        console.log(threeFirstWords)
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                console.log(response)
+                const { url } = response
+                setImageUrl(url)
+            })
+    }, [fact])
+    return imageUrl
+}
 
 export function App() {
-    const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-    // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firtsWord}?size=50&color=red&&json=true`
     const [fact, setFact] = useState()
+    const imageUrl = useCatImage({fact})
+    
+    //Recuperar cita al cargar la página
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const {fact} = data
-                setFact(fact)
-                const threeFirstWords = fact.split(' ', 3).join(' ')
-                console.log(threeFirstWords)
-                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&&json=true`)
-                    .then(res => res.json())
-                    .then(response => {
-                        console.log(response) 
-                        const {url} = response
-                    })
-            })
-    }, [])
+        getRandomFact().then(setFact)   //Puede ser con .then o con await como en handleClick
+        // getRandomFact().then(newFact => setFact(newFact))   
+
+    }, []) 
+
+    
+
+    const handleClick = async () => {
+        const newFact = await getRandomFact()
+        setFact(newFact)
+    }
+
     return (
         <main>
             <h1>App de gatitos</h1>
-            {fact && <p>{fact}</p>}
+            <button onClick={handleClick}>Get new Fact</button>
+            <section>
+                {fact && <p>{fact}</p>}
+                {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+            </section>
         </main>
     )
+
 }
---
+++ projects/prueba_tecnica/src/services/fact.js
@@ -0,0 +1,12 @@
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+export const getRandomFact = async () => {
+    try {
+        const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+        if (!res.ok) throw new Error('Error fetching fact: ' + res.message)
+        const data = await res.json()
+        const { fact } = data
+        return fact
+    } catch (error) {
+        return console.error('Error:', error)
+    }
+}
